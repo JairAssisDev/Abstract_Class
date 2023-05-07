91d5d9247717c981f665d536c5f2a8634f4648ff
Jair Victor Dev <63867699+JairAssisDev@users.noreply.github.com>
2023-05-06 21:10:31 -0300

    Update App.java

--- App.java
+++ App.java
@@ -24,22 +24,22 @@ public class App {
 			System.out.println("2 - para Aumentar o salario dos funcionarios");
 			System.out.println("0 - para sair do programa");
 			optionUser = ler.nextInt();
-			System.out.println("---_----------_----------_--------_------");
+			System.out.println("---_----------_----------_----------");
 			if(optionUser==1){
 				System.out.println("Voce deseja imprimir os dados do Gerente ou do Programador ?");
 				System.out.println("1 - para Gerente");
 				System.out.println("2 - para Programador");
 				inprimir_dados = ler.nextInt();
-				System.out.println("---_----------_----------_--------_------");
+				System.out.println("---_----------_----------_----------")
 				if(inprimir_dados==1){
 					System.out.println("Nome: "+gerente.getNome());
 					System.out.println("Salario: "+ gerente.getSalario());
-					System.out.println("---_----------_----------_--------_------");
+					System.out.println("---_----------_----------_----------")
 				}
 				if (inprimir_dados==2) {
 					System.out.println("Nome: "+programador.getNome());
 					System.out.println("Salario: "+ programador.getSalario());
-					System.out.println("---_----------_----------_--------_------");
+					System.out.println("---_----------_----------_----------")
 				}
 			}
 			if(optionUser==2){
@@ -47,17 +47,17 @@ public class App {
 				System.out.println("1 - para Gerente");
 				System.out.println("2 - para Programador");
 				aumentar_salario = ler.nextInt();
-				System.out.println("---_----------_----------_--------_------");
+				System.out.println("---_----------_----------_----------")
 				if(aumentar_salario==1){
 					gerente.aumentasalario(1.10);
 					System.out.println("Nome: "+gerente.getNome()+"Salario com os 10% de almento  "+ gerente.getSalario());
-					System.out.println("---_----------_----------_--------_------");
+					System.out.println("---_----------_----------_----------")
 
 				}
 				if (aumentar_salario==2) {
 					programador.aumentasalario(1.20);
 					System.out.println("Nome: "+programador.getNome()+"Salario com os 10% de almento  "+ programador.getSalario());
-					System.out.println("---_----------_----------_--------_------");
+					System.out.println("---_----------_----------_----------")
 
 					}
 
