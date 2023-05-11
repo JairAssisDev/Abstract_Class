480b3f7aab59b64c532d304539b4b350b4c29403
Jair Victor Dev <63867699+JairAssisDev@users.noreply.github.com>
2023-05-11 15:04:21 -0300

    Add files via upload

--- aulaabstract1/App.java
+++ aulaabstract1/App.java
@@ -6,26 +6,46 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ProfessorAssociado associado = new ProfessorAssociado();
-		associado.setNome("Jair");
-		associado.setSalario(2000);
-		ProfessorHorista horista = new ProfessorHorista();
-		horista.setNome("jao");
-		horista.setSalario(1900);
+		ProfessorAssociado associado1 = new ProfessorAssociado();
+		associado1.setNome("Jair");
+		associado1.setSalario(2000);
+		ProfessorAssociado associado2 = new ProfessorAssociado();
+		associado2.setNome("victor");
+		associado2.setSalario(2000);
+		ProfessorAssociado associado3 = new ProfessorAssociado();
+		associado3.setNome("eve");
+		associado3.setSalario(2000);
 		
-		associado.aumentasalario(20);
-		horista.aumentasalario(30);
+		ProfessorHorista horista1 = new ProfessorHorista();
+		horista1.setNome("jao");
+		horista1.setSalario(1900);
+		ProfessorHorista horista2 = new ProfessorHorista();
+		horista2.setNome("jao");
+		horista2.setSalario(1900);
+		ProfessorHorista horista3 = new ProfessorHorista();
+		horista3.setNome("jao");
+		horista3.setSalario(1900);
 		
-		List<String> listadeprofessor = new ArrayList<>();
-		listadeprofessor.add("Nome: "+associado.getNome()+" Salario: "+associado.getSalario());
-		listadeprofessor.add("Nome: "+horista.getNome()+" Salario: "+horista.getSalario());
+		associado1.aumentasalario(20);
+		horista2.aumentasalario(30);
 		
-		for (String prof : listadeprofessor) {
-		    System.out.println("Aula: " + prof);
+		List<ProfessorHorista> listadeprofessor = new ArrayList<ProfessorHorista>();
+		listadeprofessor.add(horista1);
+		listadeprofessor.add(horista2);
+		listadeprofessor.add(horista3);
+		
+		
+		for (ProfessorHorista prof : listadeprofessor) {
+		    System.out.println("nome: " + prof.getNome()+prof.getSalario());
 		}
 		
+		List<ProfessorAssociado> lAssociados = new ArrayList<ProfessorAssociado>();
+		lAssociados.add(associado1);
+		lAssociados.add(associado2);
+		lAssociados.add(associado3);
 		
-		
+		for (ProfessorAssociado prof : lAssociados) {
+			System.out.println("nome: " + prof.getNome()+prof.getSalario());		}
 		
 	}	
 	
